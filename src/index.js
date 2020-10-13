3a8db5717528c6a617be3ebf349813a156405630
Ctouchdev <63693926+Ctouchdev@users.noreply.github.com>
2020-10-13 17:02:31 -0500

    Updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img1 = "https://picsum.photos/200";
+const img2 = "https://picsum.photos/200";
+const img3 = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img className="images" alt="Random" src={img1 + "?grayscale"} />
+    <img className="images" alt="Random2" src={img2 + "?sepia"} />
+    <img className="images" alt="Random3" src={img3} />
   </div>,
   document.getElementById("root")
 );
